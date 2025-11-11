b38b10166da745b9c9bb38da7ab743cd12963f9c
newguinea04 <s22310098@student.unklab.ac.id>
2025-11-11 15:35:22 +0800

    add SignIn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import SignUp from './src/pages/SignUp';
+import SignIp from './src/pages/SignIn';
 
-AppRegistry.registerComponent(appName, () => SignUp);
+AppRegistry.registerComponent(appName, () => SignIp);
