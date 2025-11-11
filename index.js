beb15e5918352dc13f486f29baf42ec6fdb32477
newguinea04 <s22310098@student.unklab.ac.id>
2025-11-11 15:56:05 +0800

    update logo onBoarding1 & onBoarding2

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import onBoarding1 from './src/pages/onBoarding';
+import onBoarding from './src/pages/onBoarding';
 
-AppRegistry.registerComponent(appName, () => onBoarding1);
+AppRegistry.registerComponent(appName, () => onBoarding);
